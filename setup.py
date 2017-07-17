f0bee83d0c60f6f91cec79bbff91d82c1fdb27fd
Fabian <fabianjul@gmail.com>
2017-07-17 11:09:08 +0200

    Initial

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='fjcommon',
+      version='0.1',
+      description='Python code usable accross projects',
+      url='https://github.com/fab-jul/fjcommon',
+      author='fab-jul',
+      author_email='fabianjul@gmail.com',
+      license='MIT',
+      packages=['fjcommon'],
+      zip_safe=False)
