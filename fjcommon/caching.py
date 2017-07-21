9ef4a3940aa0213d2ded784efaf8814710a2b4e9
Fabian <fabianjul@gmail.com>
2017-07-21 11:31:29 +0200

    comments

--- fjcommon/caching.py
+++ fjcommon/caching.py
@@ -3,6 +3,11 @@ import pickle
 
 
 def cached(cache_path, generator):
+    """
+    Takes a function `generator` that takes no arguments and returns any pickle-able object. Checks if the output of
+    a previous call to that function has been stored at `cache_path`. If so, returns that output, otherwise calls
+    `generator` to generate the output and save it at `cache_path`.
+    """
     if path.exists(cache_path):
         with open(cache_path, 'rb') as f:
             return pickle.load(f)
@@ -14,8 +19,18 @@ def cached(cache_path, generator):
 
 def cache(cache_path):
     """
-    Decorator. Decorates a function that takes no arguments and returns any pickle-able object. Useful for when
-    `cache_path` is static.
+    Decorator version of `cached`. Useful for when `cache_path` is static.
+    The following two are equivalent:
+
+    @cache('cache_path')
+    def foo():
+        return 42
+
+    def foo():
+        def _get():
+            return 42
+        return cached('cache_path', _get)
+
     """
     def cache_decorator(generator):
         def wrapper():
