152b8a4a1f78e40a70da5924065ac63dc40fcfbf
Fabian <fabianjul@gmail.com>
2017-07-21 11:32:47 +0200

    version bump: 0.1.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='fjcommon',
       packages=['fjcommon'],
-      version='0.1.2',
+      version='0.1.3',
       author='fab-jul',
       author_email='fabianjul@gmail.com',
       description='Python code usable accross projects',
