ad6bee48c2438390804da11a2386aff10e603326
Fabian <fabianjul@gmail.com>
2017-07-24 16:18:41 +0200

    rename: tensorflow -> tf_helpers tf_helpers: added log_values added histogram_nd

--- fjcommon/tf_helpers.py
+++ fjcommon/tf_helpers.py
@@ -6,7 +6,7 @@ from collections import OrderedDict
 import os
 from os import path
 import pickle
-from . import lifting
+from fjcommon import lifting
 
 # Session ----------------------------------------------------------------------
 
@@ -137,6 +137,14 @@ def get_default_logger():
     return _default_logger
 
 
+def log_values(summary_writer, tags_and_values, iteration):
+    summary_values = [
+        tf.Summary.Value(tag=tag, simple_value=value)
+        for tag, value in tags_and_values
+    ]
+    summary_writer.add_summary(tf.Summary(value=summary_values), global_step=iteration)
+
+
 # Saving -----------------------------------------------------------------------
 
 
@@ -145,16 +153,20 @@ class VersionAwareSaver(object):
         os.makedirs(save_dir, exist_ok=True)
         self.save_path = path.join(save_dir, 'ckpt')
         self.var_names_fn = path.join(save_dir, 'var_names.pkl')
+        self.init_unrestored_op = None
 
         current_vars = (tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES) +
                         tf.get_collection(tf.GraphKeys.SAVEABLE_OBJECTS))
         if path.exists(self.var_names_fn):
             restorable_var_names = self._get_restorable_var_names()
             var_list = [var for var in current_vars if var.name in restorable_var_names]
+            # TODO untested:
             if len(var_list) != len(current_vars):
-                tf.logging.warn('Graph holds {} variables, restoring {}...'.format(
-                    len(current_vars), len(var_list)))
-            # TODO; init un-restored vars
+                tf.logging.warn('Graph holds {} variables, restoring {}...'.format(len(current_vars), len(var_list)))
+                unrestored = [var for var in current_vars if var.name not in restorable_var_names]
+                if unrestored:
+                    tf.logging.warn('Not restored: {}'.format(unrestored))
+                    self.init_unrestored = tf.initialize_variables(unrestored)
         else:
             var_list = current_vars
             self._set_restorable_var_names([var.name for var in current_vars])
@@ -165,8 +177,12 @@ class VersionAwareSaver(object):
         self.saver.save(sess, self.save_path, global_step)
 
     def restore(self, sess):
+        """ Restores variables and initialized un-restored variables. """
         latest_ckpt = tf.train.latest_checkpoint(path.dirname(self.save_path))
         self.saver.restore(sess, latest_ckpt)
+        # TODO untested:
+        if self.init_unrestored is not None:
+            sess.run(self.init_unrestored)
 
     def _get_restorable_var_names(self):
         assert path.exists(self.var_names_fn)
@@ -178,5 +194,62 @@ class VersionAwareSaver(object):
         with open(self.var_names_fn, 'wb') as f:
             return pickle.dump(var_names, f)
 
+
+# Histogram --------------------------------------------------------------------
+
+
+def histogram_nd(name, values, L, num_rows=100, initializer=tf.ones_initializer()):
+    """
+    :param name: name of the histogram variable
+    :param values: tensor of dtype int64. Will do histogram over last dimension (channel dimension).
+    :param L: number of possible values in `values`.
+    :param num_rows: number of rows of the histogram
+    :return: (histogram, update_op), where
+        histogram: tensor of dimension (num_rows, C, L), where C = values.shape[-1]
+        update_op: operation to run to update the histogram from the `values` tensor
+    """
+    assert tf.int64.is_compatible_with(values.dtype), 'values must be int64, not {}'.format(values.dtype)
+
+    C = values.get_shape().as_list()[-1]
+    histogram = tf.get_variable(
+        name, shape=(num_rows, C, L), dtype=tf.int64, initializer=initializer, trainable=False)
+    histogram_current_idx = get_variable_zeros(name + '_idx', shape=(), dtype=tf.int64)
+    histogram_current_idx_inc = tf.assign(histogram_current_idx,
+                                          tf.mod(histogram_current_idx + 1, num_rows))
+    # one row of the histogram, (C, L) dimensional
+    histo_slice = _histogram_slice(values, C, L)
+
+    with tf.control_dependencies([histogram_current_idx_inc]):
+        # write histo_slice to the current index in the histogram
+        update_op = tf.scatter_update(histogram, histogram_current_idx, histo_slice)
+        
+    return histogram, update_op
+
+
+def _histogram_slice(values, C, L):
+    # necessary to work around TF expecting floats for histogram_fixed_width,
+    # see https://github.com/tensorflow/tensorflow/issues/6650
+    values = tf.to_float(values)
+    histo_vals = tf.constant([0, L - 1], dtype=tf.float32)
+    # (C, L)
+    histo_slice = tf.stack([
+        tf.histogram_fixed_width(values=values[..., channel], value_range=histo_vals, nbins=L)
+        for channel in range(C)], axis=0)
+    return tf.cast(histo_slice, tf.int64)
+
+
+def _test_histogram_nd():
+    a = [[0, 1, 2, 3],
+         [0, 1, 2, 3],
+         [4, 4, 4, 4]]
+    L = np.max(a) + 1
+    a = tf.constant(a, dtype=tf.int64)
+    histogram, update_op = histogram_nd('histo', a, L, num_rows=4)
+
+    s = tf.InteractiveSession()
+    s.run(tf.global_variables_initializer())
+    for _ in range(10):
+        histo_, _ = s.run([histogram, update_op])
+        print(np.sum(histo_, 0))
 
 
