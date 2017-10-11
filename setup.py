67e63c181438ebde9b1f63509d659e7dcd784fb4
Fabian <fabianjul@gmail.com>
2017-10-11 16:19:27 +0200

    made add_to_console more general (version: 0.1.8)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='fjcommon',
       packages=['fjcommon'],
-      version=0.1.8
+      version='0.1.8',
       author='fab-jul',
       author_email='fabianjul@gmail.com',
       description='Python code usable accross projects',
