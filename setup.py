49bdf079dbb9b86cad091fefabee1573e2e59d2a
Fabian <fabianjul@gmail.com>
2017-10-17 14:58:55 +0200

    more intuitive identity fn (version: 0.1.26)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='fjcommon',
       packages=['fjcommon'],
-      version='0.1.25',
+      version='0.1.26',
       author='fab-jul',
       author_email='fabianjul@gmail.com',
       description='Python code usable accross projects',
