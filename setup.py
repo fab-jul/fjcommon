8e981bcf4b3fcb2dee5d08ac16bc972b160c1ad3
Fabian <fabianjul@gmail.com>
2017-11-13 11:30:45 +0100

    skip_vars for VersionAwareSaver (version: 0.1.49)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='fjcommon',
       packages=['fjcommon'],
-      version='0.1.48',
+      version='0.1.49',
       author='fab-jul',
       author_email='fabianjul@gmail.com',
       description='Python code usable accross projects',
