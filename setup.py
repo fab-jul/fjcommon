db0e0e050619ee404f5a035031b6b412b66c5382
Fabian <fabianjul@gmail.com>
2018-02-05 15:32:44 +0100

    gsuba git, MutableVar (version: 0.1.53)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='fjcommon',
       packages=['fjcommon'],
-      version='0.1.52',
+      version='0.1.53',
       author='fab-jul',
       author_email='fabianjul@gmail.com',
       description='Python code usable accross projects',
