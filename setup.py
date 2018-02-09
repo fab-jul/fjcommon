34cef75c62fdbe81b8c0bc7d2540704af0213baf
Fabian <fabianjul@gmail.com>
2018-02-09 13:31:16 +0100

    0.1.54 (version: 0.1.54)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='fjcommon',
       packages=['fjcommon'],
-      version='0.1.53',
+      version='0.1.54',
       author='fab-jul',
       author_email='fabianjul@gmail.com',
       description='Python code usable accross projects',
