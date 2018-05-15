a969c2fb846fd26162858a496c31e7e4fcbaf1da
Fabian <fabianjul@gmail.com>
2018-05-15 08:41:31 +0200

    tf_records (version: 0.1.66)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='fjcommon',
       packages=['fjcommon'],
-      version='0.1.65',
+      version='0.1.66',
       author='fab-jul',
       author_email='fabianjul@gmail.com',
       description='Python code usable accross projects',
