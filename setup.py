70adb7204a24b8be975c8f87b132e6623b2b519d
Fabian <fabianjul@gmail.com>
2018-05-18 11:26:52 +0200

    tf_records added --assert_size (version: 0.1.69)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='fjcommon',
       packages=['fjcommon'],
-      version='0.1.68',
+      version='0.1.69',
       author='fab-jul',
       author_email='fabianjul@gmail.com',
       description='Python code usable accross projects',
