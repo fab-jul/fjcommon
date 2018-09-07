735017057dfc61ce753cb8db3b23f1d3e08977d7
Fabian <fabianjul@gmail.com>
2018-09-07 10:37:09 +0200

    updated iterable_ext.py: changed function names to be consistent, updated docs

--- fjcommon/iterable_ext.py
+++ fjcommon/iterable_ext.py
@@ -2,16 +2,18 @@ import math
 
 
 def chunks(l, num_chunks):
-    """ :returns iterator going over list l in num_chunks chunks. the last chunk may be shorter """
+    """ :returns: iterator going over list `l` in `num_chunks` chunks. the last chunk may be shorter """
     max_per_chunk = math.ceil(len(l) / num_chunks)
     for c in range(num_chunks - 1):
         yield l[c * max_per_chunk:(c+1) * max_per_chunk]
     yield l[(num_chunks - 1) * max_per_chunk:]  # last chunk may be shorter
 
 
-def flag_first_iter(it):
-    """ :returns an iterator yielding tuples (first, el), where first is True for the first element and False
-    otherwise. """
+def flag_first(it):
+    """
+    :returns: an iterator yielding tuples (first, el), where first is True for the first element and False
+    otherwise.
+    """
     it = iter(it)
     try:
         first_el = next(it)
@@ -22,8 +24,11 @@ def flag_first_iter(it):
         yield False, el
 
 
-def sliced_iter(it, slice_len, allow_smaller_final_slice=True):
-    """ :returns iterator going over it in slices of length slice_len. """
+def slices(it, slice_len, allow_smaller_final_slice=True):
+    """
+    :returns: iterator going over it in (non-overlapping) slices of length slice_len. Like chunks() but the chunk
+    size is specified rather than the number of chunks
+    """
     assert slice_len > 0
     current_slice = []
     for el in it:
@@ -36,17 +41,18 @@ def sliced_iter(it, slice_len, allow_smaller_final_slice=True):
 
 
 def printing_iterator(it, first_n=5):
-    """ :returns it but as a side effect prints the first first_n elements in it. """
+    """ :returns: it but as a side effect prints the first first_n elements in it. """
     for i, el in enumerate(it):
         if i < first_n:
             print(el)
         yield el
 
 
-def iter_with_sliding_window(it, window_size):
-    """ :returns iterator going over it with a moving window of size window_size.
+def sliding_window(it, window_size):
+    """ :returns: iterator going over it with a moving window of size window_size. i.e. like sliced_iter but with
+    overlapping slices
     Example:
-    >>> it = iter_with_sliding_window(range(5), 3)
+    >>> it = sliding_window(range(5), 3)
     >>> next(it) -> (0, 1, 2)
     >>> next(it) -> (1, 2, 3)
     >>> next(it) -> (2, 3, 4)
@@ -65,6 +71,7 @@ def iter_with_sliding_window(it, window_size):
 
 
 def get_element_at(element_idx, it):
+    """ :return it[element_idx] also for non-indexable iterators. NOTE: exhausts iterator! """
     try:
         return it[element_idx]
     except TypeError:  # it is not a list
@@ -73,3 +80,14 @@ def get_element_at(element_idx, it):
         if i == element_idx:
             return el
     raise IndexError('Iterator does not have {} elements'.format(element_idx))
+
+
+def filter_split(f, it):
+    """
+    :return: two lists, a, b, where a = filter(f, it) and b = filter(not . f, it)
+    """
+    a, b = [], []
+    for el in it:
+        lst = a if f(el) else b
+        lst.append(el)
+    return a, b
