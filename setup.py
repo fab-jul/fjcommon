1ee4c9bc27754466a236cd0ad0ff1a153f501d5f
Fabian <fabianjul@gmail.com>
2018-09-07 10:50:51 +0200

    config_diff (version: 0.2.1)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='fjcommon',
       packages=['fjcommon'],
-      version='0.2',
+      version='0.2.1',
       author='fab-jul',
       author_email='fabianjul@gmail.com',
       description='Python code usable accross projects',
