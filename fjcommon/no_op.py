f5558c9ec423b4498d6dfaf74f3e0c7eb7bd0a0d
Fabian <fabianjul@gmail.com>
2019-01-17 11:06:15 +0100

    ctx mgrs for no_op

--- fjcommon/no_op.py
+++ fjcommon/no_op.py
@@ -10,6 +10,12 @@ class _NoOp(object):
         """ Means NoOp() works, but also NoOp()()()... """
         return NoOp
 
+    def __enter__(self):
+        return NoOp
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        return NoOp
+
 
 NoOp = _NoOp()
 
@@ -17,3 +23,10 @@ NoOp = _NoOp()
 def _no_op(*args, **kwargs):
     return NoOp  # return NoOp s.t. NoOp.foo().bar().baz() works.
 
+
+def test_no_op():
+    n = NoOp()
+    n.foo().bar().baz()
+
+    with n.some_ctx_mgr() as t:
+        print(t.foo())
