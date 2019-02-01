976514eddc8184df2f6d3f040521649d5ab6aed5
Fabian <fabianjul@gmail.com>
2019-02-01 16:22:28 +0100

    updates no_op (version: 0.2.9)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='fjcommon',
       packages=['fjcommon'],
-      version='0.2.8',
+      version='0.2.9',
       author='fab-jul',
       author_email='fabianjul@gmail.com',
       description='Python code usable accross projects',
