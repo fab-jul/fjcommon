546396a305bf269d7db31b5dd4e0e8e22f05fc7e
Fabian <fabianjul@gmail.com>
2019-02-01 16:19:35 +0100

    updates in config_parser and no_op (version: 0.2.8)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='fjcommon',
       packages=['fjcommon'],
-      version='0.2.7',
+      version='0.2.8',
       author='fab-jul',
       author_email='fabianjul@gmail.com',
       description='Python code usable accross projects',
