0ede729870c22294fb5e9e2ff90280523fe47393
Fabian <fmentzer@me.com>
2019-10-19 21:06:34 +0200

    Parse environment variables in parameter values. (version: 0.2.11)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='fjcommon',
       packages=['fjcommon'],
-      version='0.2.10',
+      version='0.2.11',
       author='fab-jul',
       author_email='fabianjul@gmail.com',
       description='Python code usable accross projects',
